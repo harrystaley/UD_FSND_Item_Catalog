e7b03b57abc8f73d2ebeefbf275c23563748bdb3
Harry Staley <staleyh@gmail.com>
2016-06-25 14:42:59 -0500

    FEAT: FLASK Routing Quiz Solution

--- FSND-vm/vagrant/restaurant/project.py
+++ FSND-vm/vagrant/restaurant/project.py
@@ -16,10 +16,10 @@ session = DBSession()
 
 
 @app.route('/')
-@app.route('/restaurant')
-def TestRestaurant():
+@app.route('/restaurant/<int:restaurant_id>/')
+def GetRestaurant(restaurant_id):
     """ This a test function for the display of the flask implementation """
-    restaurant = session.query(Restaurant).first()
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     output = ''
     for item in items:
@@ -32,8 +32,25 @@ def TestRestaurant():
         output += item.price
         output += "<br>"
         output += "<br>"
-
     return output
+
+
+# Task 1: Create route for newMenuItem function here
+@app.route('/restaurant/<int:restaurant_id>/newitem')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+
+# Task 2: Create route for editMenuItem function here
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+
+# Task 3: Create a route for deleteMenuItem function here
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
 
 if __name__ == '__main__':
     app.debug = True
