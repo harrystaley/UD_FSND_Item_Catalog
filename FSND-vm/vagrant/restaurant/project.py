c3e201becb1a7d8b5155d4affa308a2735a64311
Harry Staley <staleyh@gmail.com>
2016-06-25 14:19:32 -0500

    FEAT: basic flask application to pull menu items from the first restaurant

--- FSND-vm/vagrant/restaurant/project.py
+++ FSND-vm/vagrant/restaurant/project.py
@@ -1,11 +1,32 @@
+
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+
+__author__ = "Harry Staley <staleyh@gmail.com>"
+__version__ = "1.0"
+
 app = Flask(__name__)
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    return "Hello World!"
+@app.route('/restaurant')
+def TestRestaurant():
+    """ This a test function for the display of the flask implementation """
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    output = ''
+    for item in items:
+        output += item.name
+        output += "<br>"
+
+    return output
 
 if __name__ == '__main__':
     app.debug = True
